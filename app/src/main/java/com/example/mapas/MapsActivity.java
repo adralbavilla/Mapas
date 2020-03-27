d4b4522ae72e93dc55cb02663220c8de0914a35e
adralbavilla <adralbavilla@gmail.com>
2020-03-27 02:50:54 -0600

    Provider Mapas

--- app/src/main/java/com/example/mapas/MapsActivity.java
+++ app/src/main/java/com/example/mapas/MapsActivity.java
@@ -5,12 +5,16 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -27,7 +31,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import java.io.IOException;
 import java.util.Locale;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener{
 
     private GoogleMap mMap;
     private Location location;
@@ -36,6 +40,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private Geocoder geocoder;
     private final String TAG = getClass().getSimpleName();
     private Address address;
+    private double lat, lon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,13 +49,36 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
+        //this.checkGPSEnabled();
         getPermission();
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //this.checkGPSEnabled();
+    }
+
+    private void checkGPSEnabled() {
+        try{
+            int gpsSignal = Settings.Secure.getInt(this.getContentResolver(),Settings.Secure.LOCATION_MODE);
+            if(gpsSignal == 0){
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+
+        }catch (Settings.SettingNotFoundException e){
+            e.printStackTrace();
+        }
     }
 
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        getPermission();
         mMap.addMarker(new MarkerOptions().position(latLng));
 /*
         CameraPosition camera = new CameraPosition.Builder()
@@ -101,21 +129,50 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             return;
         }else{
 
-            LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-            if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+            LocationManager locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+            boolean isGPS = isGpsEnabled(this);
+            boolean isNetwork = isNetworkEnabled(this);
+            /*if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
                 if(locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)!=null){
                     location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
                 }
 
             }
             if(locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)){
+
+
+
+
                 if(locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)!=null)
                     location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            }*/
+            if (location != null)
+            {
+                lat = location.getLatitude();
+                lon = location.getLongitude();
             }
-            setLatLng(location.getLatitude(),location.getLongitude());
-            mapFragment.getMapAsync(this);
+            //Request update as location manager can return null otherwise
+            else
+            {
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, this);
+            }
+            setLatLng(lat,lon);
         }
     }
+
+    public static boolean isGpsEnabled(Context context) {
+        LocationManager lm = (LocationManager) context
+                .getSystemService(Context.LOCATION_SERVICE);
+        return lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+    }
+    public static boolean isNetworkEnabled(Context context) {
+        LocationManager lm = (LocationManager) context
+                .getSystemService(Context.LOCATION_SERVICE);
+        return lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }
+
+
 
     private LatLng setLatLng(double latitude, double longitude){
         latLng = new LatLng(latitude,longitude);
@@ -136,5 +193,26 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 return;
             }
         }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        lat = location.getLatitude();
+        lon = location.getLongitude();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 }
